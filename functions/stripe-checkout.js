f6af7cbec19d5c91b394fd2da730cb6b55090dfb
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-18 00:18:48 +0100

    Trying to make stripe work

--- functions/stripe-checkout.js
+++ functions/stripe-checkout.js
@@ -1,48 +1,40 @@
-const Stripe = require("stripe");
-const product = "prod_POUjle9E7LDBwk";
+import Stripe from "stripe/lib/stripe.js";
 
-export async function onRequest(context) {
-    const stripe = new Stripe(context.env.STRIPE_SECRET_KEY);
-    if (context.request.method === "POST") {
-        const formData = await context.request.formData();
-        const body = Object.fromEntries(formData.entries());
+// use web crypto
+export const webCrypto = Stripe.createSubtleCryptoProvider();
 
-        const origin = context.request.headers.get("origin") || "http://localhost:4321";
-        const success_url = `${origin}/thankyou`;
-        const cancel_url = `${origin}/cancel`;
-
-        try {
-            const session = await stripe.checkout.sessions.create({
-                mode: body.interval === "one-time" ? "payment" : "subscription",
-                line_items: [
-                    {
-                        price_data: {
-                            currency: body.currency.toLowerCase(),
-                            recurring:
-                                body.interval === "one-time"
-                                    ? undefined
-                                    : {
-                                          interval: body.interval,
-                                      },
-                            unit_amount: body.amount * 100,
-                            product,
-                        },
-                        quantity: 1,
-                    },
-                ],
-                success_url,
-                cancel_url,
-            });
-            return new Response(JSON.stringify(session), { status: 200 });
-        } catch (error) {
-            if (error instanceof Stripe.errors.StripeError) {
-                const { message } = error;
-                return new Response(JSON.stringify({ message }), { status: error.statusCode });
-            } else {
-                return new Response("Internal server error", { status: 500 });
-            }
-        }
-    } else {
-        return new Response("Method not allowed", { status: 405 });
+export function getStripe({ env }) {
+    if (!env?.STRIPE_SECRET_KEY) {
+        throw new Error("Can not initialize Stripe without STRIPE_KEY");
     }
+    const client = Stripe(env.STRIPE_SECRET_KEY, {
+        httpClient: Stripe.createFetchHttpClient(), // ensure we use a Fetch client, and not Node's `http`
+    });
+    return client;
 }
+
+export default {
+    async fetch(request, env) {
+        const stripe = getStripe({ env });
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: "T-shirt",
+                        },
+                        unit_amount: 2000,
+                    },
+                    quantity: 1,
+                },
+            ],
+            payment_method_types: ["card"],
+            mode: "payment",
+            success_url: `${YOUR_DOMAIN}/success.html`,
+            cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+        });
+
+        return Response.redirect(session.url);
+    },
+};
