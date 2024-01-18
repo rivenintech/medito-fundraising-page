17df9c26b69bd5101191d9884b277bcb1ef82bb4
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-18 02:37:19 +0100

    Added all Stripe supported currencies & custom form data

--- functions/stripe-checkout.ts
+++ functions/stripe-checkout.ts
@@ -11,6 +11,30 @@ interface Body {
     interval: string; // 'month' | 'year' | 'payment (one-time)'
 }
 
+// Format the amount to the correct number of decimal places for the currency
+function formatAmount(currency, amount) {
+    const threeDecimalCurrencies = ["bhd", "jod", "kwd", "omr", "tnd"];
+    const zeroDecimalCurrencies = [
+        "bif", "clp", "djf", "gnf", "jpy", "kmf", "krw", "mga", "pyg", "rwf",
+        "ugx", "vnd", "vuv", "xaf", "xof", "xpf"
+    ];
+
+    const isZeroDecimal = zeroDecimalCurrencies.includes(currency.toLowerCase());
+    const isThreeDecimal = threeDecimalCurrencies.includes(currency.toLowerCase());
+
+    let formattedAmount;
+
+    if (isZeroDecimal) {
+        return parseInt(amount.toFixed(0));
+    } else if (isThreeDecimal) {
+        // Round to the nearest ten for three-decimal currencies
+        return Math.round(amount / 10) * 10;
+    } else {
+        // Standard two-decimal currencies
+        return parseInt((amount * 100).toFixed(0));
+    }
+}
+
 export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
     try {
         const body = await request.json<Body>();
@@ -36,7 +60,7 @@ export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
                                 : {
                                     interval: body.interval,
                                 },
-                        unit_amount: body.amount * 100,
+                        unit_amount: formatAmount(body.currency, body.amount),
                         product: env.PRODUCT_ID,
                     },
                     quantity: 1,
@@ -49,7 +73,6 @@ export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
         return new Response(JSON.stringify({ success: true, data: { redirect_url: session.url } }));
     } catch (e) {
         return new Response(
-            // @ts-ignore
             JSON.stringify({ success: false, error: "Payment failed - " + e.message }),
             { status: 500 }
         );
