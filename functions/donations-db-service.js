3baab4a4957e1705c0147e4058acb59008f9461d
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-23 22:28:35 +0100

    Update donations-db-service.js

--- functions/donations-db-service.js
+++ functions/donations-db-service.js
@@ -1,20 +1,21 @@
-import { Router } from "itty-router";
 import Stripe from "stripe";
 
-const router = Router();
-
-router.post("/", async (req, env) => {
+export async function onRequestPost(context) {
     console.log("Received a POST request to");
-    const stripe = Stripe(env.STRIPE_API_KEY, {
-        httpClient: Stripe.createFetchHttpClient(),
-    });
-    const signature = req.headers.get("stripe-signature");
+    const stripe = new Stripe(context.env.STRIPE_API_KEY);
+    const signature = context.request.headers.get("stripe-signature");
     try {
         if (!signature) {
             return new Response("", { status: 400 });
         }
-        const body = await req.text();
-        const event = await stripe.webhooks.constructEventAsync(body, signature, env.STRIPE_WEBHOOK_SECRET, undefined, Stripe.createSubtleCryptoProvider());
+        const body = await context.request.text();
+        const event = await stripe.webhooks.constructEventAsync(
+            body,
+            signature,
+            context.env.STRIPE_WEBHOOK_SECRET,
+            undefined,
+            Stripe.createSubtleCryptoProvider()
+        );
         switch (event.type) {
             case "charge.succeeded": {
                 const data = event.data.object;
@@ -23,7 +24,7 @@ router.post("/", async (req, env) => {
                 const exchange_rate = transaction.exchange_rate || 1; // Assign default value of 1 if exchange_rate is null
                 const amount = Math.round(data.amount * exchange_rate); // Convert the amount to the currency of the Stripe account
                 // Insert the donation into the database
-                await env.DONATIONS_DB.prepare("INSERT INTO donations (id, amount, donor_name, donor_email) VALUES (?1, ?2, ?3, ?4)")
+                await context.env.DONATIONS_DB.prepare("INSERT INTO donations (id, amount, donor_name, donor_email) VALUES (?1, ?2, ?3, ?4)")
                     .bind(data.id, amount, donor_details.name, donor_details.email)
                     .run();
                 break;
@@ -37,4 +38,4 @@ router.post("/", async (req, env) => {
         console.log(errorMessage);
         return new Response(errorMessage, { status: 400 });
     }
-});
+}
