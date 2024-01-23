86b740e0545a17d1cdbc467690fb3c00f186bd2e
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-24 00:40:47 +0100

    Simplified the syntax

--- functions/donations.js
+++ functions/donations.js
@@ -4,11 +4,8 @@ import { Router, json } from "itty-router";
 const router = Router();
 
 // Get the total amount raised and the number of donations
-router.get("/progress", async (req, env, ctx) => {
-    const data = await env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first();
-    return data;
-});
+router.get("/progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first());
 
 export default {
-    fetch: async (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error),
+    fetch: (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error),
 };
