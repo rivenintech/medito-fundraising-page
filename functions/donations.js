ec8108f7a8a1d6fd9c8edad3ba4f9e28e921d6b4
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-24 13:58:44 +0100

    Fixing donations.js not being deployed on CF

--- functions/donations.js
+++ functions/donations.js
@@ -1,11 +1,9 @@
 import { Router, error, json } from "itty-router";
 
 // Create a router instance
-const router = Router((base = "/donations"));
+const router = Router();
 
 // Get the total amount raised and the number of donations
 router.get("/progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first());
 
-export default {
-    fetch: (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error),
-};
+export const onRequest = (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error);
