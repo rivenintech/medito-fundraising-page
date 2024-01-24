aeff04827feb8802442c2d10d3c66b2aa05d16e1
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-24 14:21:04 +0100

    Fixing routes

--- functions/donations.js
+++ functions/donations.js
@@ -1,12 +1,14 @@
-import { Router, error, json } from "itty-router";
+import { Router } from "itty-router";
 
 // Create a router instance
-const router = Router({ base: "donations" });
+const router = Router();
 
 // Get the total amount raised and the number of donations
 router
-    .get("progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first())
+    .get("/progress", async (request, env) => await env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first())
+    .get("/", async (request, env) => "Hello World!");
 
-    .all("*", () => error(404));
-
-export const onRequest = (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error);
+export const onRequest = async (context) => {
+    const { request, env } = context;
+    return await router.handle(request, env);
+};
