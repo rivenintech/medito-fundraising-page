ef2feb254fc14ae5ce56cdabd655db992c5aa840
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-24 14:49:52 +0100

    Added 404 path, and missing imports

--- functions/donations/[path].js
+++ functions/donations/[path].js
@@ -1,9 +1,11 @@
-import { Router } from "itty-router";
+import { Router, error, json } from "itty-router";
 
 // Create a router instance
 const router = Router();
 
 // Get the total amount raised and the number of donations
-router.get("/progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first());
+router
+    .get("/progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first())
+    .all("*", () => error(404));
 
 export const onRequest = (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error);
