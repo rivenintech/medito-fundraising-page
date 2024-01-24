4a62b1400913171e84c645a6a1045248a986b315
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-24 14:40:37 +0100

    Description
    https://developers.cloudflare.com/pages/functions/routing/#dynamic-routes

---
+++ functions/donations/[path].js
@@ -0,0 +1,9 @@
+import { Router } from "itty-router";
+
+// Create a router instance
+const router = Router();
+
+// Get the total amount raised and the number of donations
+router.get("/progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first());
+
+export const onRequest = (req, env, ctx) => router.handle(req, env, ctx).then(json).catch(error);
