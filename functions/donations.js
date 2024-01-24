6ea318030d5a6f2e95b3f7c5b81e10305420a673
RiveN <61630074+rivenintech@users.noreply.github.com>
2024-01-24 01:19:05 +0100

    Update donations.js

--- functions/donations.js
+++ functions/donations.js
@@ -1,7 +1,7 @@
 import { Router, error, json } from "itty-router";
 
 // Create a router instance
-const router = Router();
+const router = Router((base = "/"));
 
 // Get the total amount raised and the number of donations
 router.get("/progress", (req, env, ctx) => env.DONATIONS_DB.prepare("SELECT raised_amount, donations_count FROM donation_progress").first());
